bb0a0cd06d2dad7a6d410be9a286d47b8ad60689
Rick Hallett <rick.hallett@brandwatch.com>
2023-11-03 11:20:43 +0000

    get ready for heroku

--- pimpmygpt/gpt.py
+++ pimpmygpt/gpt.py
@@ -3,12 +3,8 @@ import json
 import threading
 from urllib import request as py_request
 from flask import Blueprint
-from flask import flash
-from flask import g
-from flask import redirect
 from flask import render_template
 from flask import request
-from flask import url_for
 from werkzeug.exceptions import abort
 
 from pimpmygpt.auth import login_required
